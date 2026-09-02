8ee6c253285b06bce0a5e3b0ced3d0faf3623b2b
ichi010623-maker <ichi010623@gmail.com>
2026-09-02 16:43:43 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    离线缓存 + 自动更新 + 隐私保护
    ============================================ */
 
-const CACHE_VERSION = "v5.9.98";
+const CACHE_VERSION = "v5.9.99";
 const CACHE_NAME = "pm-workbench-" + CACHE_VERSION;
 const APP_SHELL_ASSETS = [];
 
